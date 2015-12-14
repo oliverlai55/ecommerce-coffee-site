d87914e2da7084b268b91b13802387e016c98403
Wei lun Lai <oliverlai55@gmail.com>
2015-12-14 15:06:39 -0500

    setting up app.js passport

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -20,7 +24,23 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'dc-4life',
+  resave: false, 
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use(express.static(path.join(__dirname, 'public')));
+//Passport Configuration
+
+
+//Mongoose
+mongoose.connect('mongodb://localhost:27017/coffee')
+
 
 app.use('/', routes);
 app.use('/users', users);
