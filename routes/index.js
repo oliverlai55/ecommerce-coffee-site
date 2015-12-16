b8001734810e83bf4485642e9adb0b785d86db26
Wei lun Lai <oliverlai55@gmail.com>
2015-12-16 10:59:37 -0500

    made login page work
    changing index.jade button link

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { username : req.session.username });
 });
 
+//////////////////////////
 //////REGISTER GET////////
 router.get('/register', function(req, res, next){
 	res.render('register',{ });
 });
 
 ////////////////REGISTER////////////////
+
 //REGISTER POST when user submits it will go through this route .post
 //it POST TO the register page
 router.post('/register', function(req, res, next){
@@ -32,79 +34,86 @@ router.post('/register', function(req, res, next){
 					//using the local method to authenticate
 					req.session.username = req.body.username;
 					//fast way to get username w/o getting in db
+					// session module of express
 					res.render('index', { username : req.session.username });
 				});
 			}
 		});
 });
 
-
 /////LOGIN GET////
+
 //Get the login page
 router.get('/login', function(req, res) {
 
 	//the user is already logged in
 	if(req.session.username){
-		resdirect('/choices');
+		res.redirect('/index');
 	}
 	//req.query.login pulls the query parameters right out of the http headers!
     //They are here and failed a login
 	if(req.query.failedlogin){
-		res.render('login', { failed: "Your username or password is incorrect."});
+		res.render('login', { failed: "Your username or password is incorrect." });
 	}
 	//They are here and aren't logged in
-	res.render('login', { user : req.user });
-}).post('/login', function(req, res, next) {
+	res.render('login', { });
+})
 
-	if(req.body.getStarted){
-		Account.register(new Account({ username : req.body.username}), req.body.password, function(err, account) {
-			if (err) {
-				return res.render('register', { err : err });
-			}
-			if(!err)
-			passport.authenticate('local')(req, res, function() {
-				req.session.username = req.body.username;
-				res.render('choices', { username : req.session.username });
-			});
-		});
-	}
 
-	if (!req.body.getStarted){
-		passport.authenticate('local', function(err, user, info){
-			if (err) {
-				return next(err); //will generate a 500 error
-			}
-			//Generate a JSON response reflecting authentication status
-			if (! user) {
-				return res.redirect('/login?failedlogin=1');
-			}
-			if (user){
-				//Passport session setup
-				passport.serializeUser(function(user, done) {
-					console.log("serializing " + user.username);
-					done(null, user);
-				});
+/////////LOGIN POST//////////
+router.post('/login', function(req, res, next) {
 
-				passport.deserializeUser(function(obj, done) {
-					console.log("deserializing " + obj);
-					done(null, obj);
-				});
-				req.session.username = user.username;
-			}
+      passport.authenticate('local', function(err, user, info) {
+        if (err) {
+          return next(err); // will generate a 500 error
+        }
+        // Generate a JSON response reflecting authentication status
+        if (! user) {
+          return res.redirect('/login?failedlogin=1');
+        }
+        if (user){
+            // Passport session setup.
+            passport.serializeUser(function(user, done) {
+              console.log("serializing " + user.username);
+              done(null, user);
+            });
 
-			return res.redirect('/choices');
-		})(req, res, next);
-	}
+            passport.deserializeUser(function(obj, done) {
+              console.log("deserializing " + obj);
+              done(null, obj);
+            });        
+            req.session.username = user.username;
+        }
+
+        return res.redirect('/');
+      })(req, res, next);
 });
-
 
 
 //// Logout////
 router.get('/logout', function(req, res) {
-	req.logout();
 	req.session.destroy();
 	res.redirect('/');
 });
+
+
+
+//////CHOICES GET////////
+/////////////////////////
+router.get('/choices', function (req, res, next){
+	//Make sure the user is logged in
+	if(req.session.username){
+		//they do belong here, proceed with page
+		//check and see if they have any set preferences already
+		res.render('choices');
+	}else{
+		res.redirect('/')
+	}
+});
+
+
+
+
 
 
 module.exports = router;
